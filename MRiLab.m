ddcca9a747681707705d6071932806b5b3b0762b
Majid <you@example.com>
2025-08-11 20:56:32 +0330

    Test Commit

--- MRiLab.m
+++ MRiLab.m
@@ -3,7 +3,7 @@
 function MRiLab
 
     warning off; % disable warning
-    
+    % my name is Majid
     [pathstr,name,ext]=fileparts(mfilename('fullpath'));
     addpath(genpath(pathstr));
     % Remove unnessary search path
